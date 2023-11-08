32269824a78dffc567d42c02e826e20e913c0230
Axiinyaa <derfii2004@gmail.com>
2023-11-08 21:02:11 +0000

    wefewf

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className='font-main my-32 mx-10'>
         <h1 className='text-7xl text-twm-highlight'>The World Machine</h1>
         <h1 className='text-3xl my-4'>a discord bot based off oneshot</h1>
-        <button onClick={() => window.location.href = '/invite'} className='bg-twm-highlight text-white p-3 rounded-lg hover:cursor-pointer hover:animate-pulse text-3xl my-5'>Invite Now</button>
+        <button onClick={() => window.location.href = '/invite'} className='bg-twm-highlight text-white p-3 rounded-lg hover:cursor-pointer hover:animate-pulse text-3xl my-5 ml-3'>Invite Now</button>
         <button onClick={() => window.location.href = 'https://store.steampowered.com/app/420530/OneShot/'} className='bg-twm-highlight text-white p-3 rounded-lg hover:cursor-pointer hover:animate-pulse text-3xl my-10'>Buy OneShot</button>
 
       </div>
