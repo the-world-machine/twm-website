e745aa5c98a89a73ce66e1079d036052f687b814
Axiinyaa <derfii2004@gmail.com>
2024-02-15 23:38:07 +0000

    awesome

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -26,8 +26,6 @@ export default function Invite() {
                     <CommandPanel image="music" title='Music' description='Play music in your server 24/7 for free, with multiple providers, such as Spotify, SoundCloud or even your own music or video files! Play Spotify playlists and albums with ease and enjoy a dynamic player so you know exactly where the music starts and stops.'/>
                     <CommandPanel image="transmissions" title='Transmissions' description='Communicate with other Discord servers using Transmissions, discover different discord servers, and perhaps make new friends. Any servers you have previously transmitted with can be called again, so you can always get back to where you left off.'/>
                     <CommandPanel image="nikogotchi" title='Nikogotchi' description="Take care of OneShot characters like a pet. You can feed them, pet them, send them off to find treasure, and MOST importantly, feed them pancakes. Otherwise they might perish. That might be bad."/>
-                    <CommandPanel image="chatgpt" title='Chat-GPT Integrations' description="Using ChatGPT, you can ask the sassy entity itself life's most important questions. Like how to make the best pancakes ever. yep."/>
-                    <CommandPanel image="explosions" title='Explosions' description="cause catastrophic devastation with one command. 💥"/>
                 </div>
             </Window>
         </Desktop>
